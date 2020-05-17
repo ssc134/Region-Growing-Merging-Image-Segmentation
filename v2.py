5d7f3e98fd15912d251ac54e987cc127a9b13c98
Saurabh Chavan <31591900+ssc134@users.noreply.github.com>
2020-05-17 15:57:50 +0530

    initial

---
+++ v2.py
@@ -0,0 +1,106 @@
+'''
+Trying to invert the image so that object to be segmented is always bright and background dark
+Decision pending
+'''
+
+import cv2
+import numpy as np
+
+mean, pixelCount, error = 0, 0, 50
+
+
+def mergeCriteriaMet(img, px, py):
+    global error
+    if img[px, py] <= mean + error and img[px,py] >= mean-error:
+        return True
+    return False
+
+
+def valid(img, seedX, seedY):
+    if seedX < 0 or seedX >= img.shape[1] or seedY < 0 or seedY >= img.shape[0]:
+        return False
+    return True
+
+
+def flood_fill(img, seedX, seedY):
+    '''
+    img = image.
+    seedX, seedY = seed coordinates.
+    mean = mean of the flood filled region. Initialised by value of pixel while calling this function.
+    pixelCount = required for recalculating mean as region grows.
+    '''
+
+    global mean, pixelCount
+
+    if not valid(img, seedX, seedY):
+        return
+    if img[seedY, seedX] == -1:
+        return
+    if not mergeCriteriaMet(img, seedX, seedY):
+        return
+
+    
+    sum = mean*pixelCount
+    pixelCount += 1
+    mean = (sum+img[seedY, seedX])/pixelCount
+    img[seedY, seedX] = -1
+    print("mean: ", mean)
+    print("-------pixelCount: ", pixelCount, "\n")
+
+    flood_fill(img, seedX-1, seedY)
+    flood_fill(img, seedX+1, seedY)
+    flood_fill(img, seedX, seedY-1)
+    flood_fill(img, seedX, seedY+1)
+    return
+
+
+ix, iy = -1, -1
+# mouse callback function
+
+
+def draw_circle(event, x, y, flags, param):
+    global ix, iy
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        ix, iy = x, y
+
+
+# Open an image and bind the function to the window
+img1 = cv2.imread('coins.jpg', 0)
+cv2.imshow("original image", img1)
+cv2.setMouseCallback('original image', draw_circle)
+
+
+#cv2.imshow('original image',img1)
+k = cv2.waitKey(0) & 0xFF
+if k == 27:
+    print(ix, iy)
+cv2.destroyAllWindows()
+
+
+# main code
+
+
+# counting white and grey pixels to be used for determining if a small portion of an image should be merged with current region or not.
+threshold = 100
+wpc, bpc = 0, 0  # whitePixelCount & blackPixelCount
+for i in range(img1.shape[0]):
+    for j in range(img1.shape[1]):
+        if img1[i, j] >= threshold:
+            bpc += 1
+        else:
+            wpc += 1
+
+
+img2 = img1.copy()
+mean = img2[ix, iy]
+flood_fill(img2, ix, iy)
+
+# replace values '-1' in img2 with zero.
+for i in range(img2.shape[0]):
+    for j in range(img2.shape[1]):
+        if img2[i, j] == -1:
+            img2[i, j] = 0
+
+cv2.imshow("new image", img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
